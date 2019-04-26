8ec4a8cb70ec6a2b6ec4432a2360be0b271e695c
Nikhil <nikhil09jadhav@gmail.com>
2019-04-26 11:45:47 +0530

    Migration and Seed to DB

--- database/migrations/2019_04_26_044244_create_products_table.php
+++ database/migrations/2019_04_26_044244_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('details');
+            $table->text('details');
             $table->integer('stock');
             $table->decimal('price', 10, 2);
             $table->integer('discount');
